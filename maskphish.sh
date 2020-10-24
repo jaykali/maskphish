8964c09a2fbe12082bb00457d1c8f22c582ffb3e
Mehmet Ali KERİMOĞLU <42123683+CYB3RMX@users.noreply.github.com>
2020-10-24 22:08:08 +0000

    Little improvements
    Removed unnecessary echo statements and input zones are upgraded.

--- maskphish.sh
+++ maskphish.sh
@@ -9,43 +9,30 @@ url_checker() {
     fi
 }
 
-echo
-echo
-echo -e "\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
+echo -e "\n\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
 echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
 echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
 echo -e "\e[1;31;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
 echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
-echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m"
-echo
+echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m \n"
 echo -e "\e[40;38;5;82m Please Visit \e[30;48;5;82m https://www.kalilinux.in \e[0m"
-echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m"
-echo
-echo
-echo -e "\e[1;31;42m ### Phishing URL ###\e[0m"
-echo
+echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m \n\n"
+echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
 echo -n "Paste Phishing URL here (with http or https): "
 read phish
 url_checker $phish
 short=$(curl -s https://da.gd/s/?url=${phish})
 shorter=${short#https://}
-echo
-echo -e "\e[1;31;42m ### Masking Domain ###\e[0m"
+echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
 echo 'Domain to mask the Phishing URL (with http or https), ex https://google.com, http
 ://anything.org) :'
-echo
+echo -en "\e[32m=>\e[0m "
 read mask
 url_checker $mask
-echo
-echo 'Type social engineering words:(like free-money, best-pubg-tricks)'
-echo
+echo -e '\nType social engineering words:(like free-money, best-pubg-tricks)'
 echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
-echo
+echo -en "\e[32m=>\e[0m "
 read words
-echo
-echo 'Generating MaskPhish Link...'
-echo
-echo 'Here is the MaskPhish URL..'
-echo
+echo -e "\nGenerating MaskPhish Link...\n"
 final=$mask-$words@$shorter
-echo $final
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
