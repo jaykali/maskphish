5139528637e19dbf14f5265a584dfde7f61f5abb
Oak atsume <56260438+OakAtsume@users.noreply.github.com>
2022-11-28 20:39:43 -0700

    Update maskphish.sh

--- maskphish.sh
+++ maskphish.sh
@@ -1,55 +1,45 @@
-# Bash Script for Hide Phishing URL Created by KP
+#!/bin/bash
 
-url_checker() {
-    if [ ! "${1//:*}" = http ]; then
-        if [ ! "${1//:*}" = https ]; then
-            echo -e "\e[31m[!] Invalid URL. Please use http or https.\e[0m"
-            exit 1
-        fi
-    fi
+function urlChecker() {
+	if [[ ! "${1//:*}" =~ ^(http|https)$ ]]; then
+		echo "\e[31m[*] Invalid URL: ${1}\e[0m"
+
+		return 0
+	fi
+	return 1
 }
 
-echo -e "\n\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
-echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
-echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
-echo -e "\e[1;31;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
-echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
-echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m \n"
-echo -e "\e[40;38;5;82m Please Visit \e[30;48;5;82m https://www.kalilinux.in \e[0m"
-echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m \n\n"
-echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
-echo -n "Paste Phishing URL here (with http or https): "
-read phish
-url_checker $phish
-sleep 1
-echo "Processing and Modifing Phishing URL"
-echo ""
-short=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${phish})
-shorter=${short#https://}
-echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
-echo 'Domain to mask the Phishing URL (with http or https), ex: https://google.com, http
-://anything.org) :'
-echo -en "\e[32m=>\e[0m "
-read mask
-url_checker $mask
-echo -e '\nType social engineering words:(like free-money, best-pubg-tricks)'
-echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
-echo -en "\e[32m=>\e[0m "
-read words
-if [[ -z "$words" ]]; then
-echo -e "\e[31m[!] No words.\e[0m"
-echo -e "\nGenerating MaskPhish Link...\n"
-final=$mask@$shorter
-echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
-exit
+printf "\e[32m[*] Enter URL: \e[0m"
+read -r url
+if [[ -z ${url} ]]; then
+	echo -e "\e[31m[*] URL cannot be empty!\e[0m"
+	exit 1
 fi
-if [[ "$words" =~ " " ]]; then
-echo -e "\e[31m[!] Invalid words. Please avoid space.\e[0m"
-echo -e "\nGenerating MaskPhish Link...\n"
-final=$mask@$shorter
-echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
-exit
+printf "Checking URL: ${url}..."
+if ! urlChecker "${url}"; then
+	echo -e "\e[32m[OK]\e[0m"
+else
+	echo -e "\e[31m[FAIL]\e[0m"
+	exit 1
 fi
-echo -e "\nGenerating MaskPhish Link...\n"
-final=$mask-$words@$shorter
-echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+printf "Let's get some keywords ex: (Free-money, Game-Cheat, etc): "
+read -r keywords
+if [[ -z ${keywords} ]]; then
+	echo -e "\e[31m[*] Keywords cannot be empty!\e[0m"
+	exit 1
+fi
+printf "Checking Keywords: ${keywords}..."
+if [[ ${keywords} =~ [[:space:]] ]]; then
+	echo -e "\e[31m[FAIL]\e[0m"
+	echo -e "\e[31m[*] Keywords cannot contain spaces!\e[0m"
+	exit 1
+else
+	echo -e "\e[32m[OK]\e[0m"
+fi
+# Generate a short url with the given url and keywords
+shortUrl=$(curl -s "https://is.gd/create.php?format=simple&url=${url}&shorturl=${keywords}")
+if [[ -z ${shortUrl} ]]; then
+	echo -e "\e[31m[*] Failed to generate short URL!\e[0m"
+	exit 1
+fi
+echo -e "\e[32m[*] Short URL: ${shortUrl}\e[0m"
